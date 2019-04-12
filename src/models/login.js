4d084cf28a409eb46603c841bfda2b93e54bdd40
kieyooo <2249733556@qq.com>
2019-04-12 10:04:13 +0800

    增加登陆提示

--- src/models/login.js
+++ src/models/login.js
@@ -4,6 +4,7 @@ import * as service from '@/services/login'
 import { setAuthority } from '@/utils/authority';
 import { getPageQuery } from '@/utils/utils';
 import { reloadAuthorized } from '@/utils/Authorized';
+import notificate from '@/utils/notification'
 
 export default {
   namespace: 'login',
@@ -15,12 +16,13 @@ export default {
   effects: {
     *login({ payload }, { call, put }) {
       const response = yield call(service.login, payload);
-      yield put({
-        type: 'changeLoginStatus',
-        payload: { currentAuthority : 'admin' },
-      });
+      
       // Login successfully
       if (response.status === 200) {
+        yield put({
+          type: 'changeLoginStatus',
+          payload: { currentAuthority : 'admin' },
+        });
         reloadAuthorized();
         const urlParams = new URL(window.location.href);
         const params = getPageQuery(); 
@@ -38,6 +40,7 @@ export default {
         }
         yield put(routerRedux.replace(redirect || '/'));
       }
+      if (response.status === 302) notificate("warning","警告","用户名或密码出错！")
     },
     *logout(_, { put }) {
       yield put({
