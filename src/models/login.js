fff2bb9b9d8d0b84ee5383b17389d02152bd9062
kieyooo <2249733556@qq.com>
2019-04-16 20:46:16 +0800

    cookie 失效返回登录界面

--- src/models/login.js
+++ src/models/login.js
@@ -43,8 +43,8 @@ export default {
       if (response.status === 404) notificate("warning","警告","用户名或密码出错！");
       if (response.status >= 500 ) notificate("error",'出错',"网络或服务器出现问题！");
     },
-    *logout(_, { put, call }) {
-      yield call(service.logout);
+    *logout(_, { put }) {
+      // yield call(service.logout);
       yield put({
         type: 'changeLoginStatus',
         payload: {
