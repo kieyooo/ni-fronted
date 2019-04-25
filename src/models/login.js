2051b3979bb1f2d0bbd3108da5b66201952d80e8
kieyooo <2249733556@qq.com>
2019-04-25 17:54:38 +0800

    更新上一次路由

--- src/models/login.js
+++ src/models/login.js
@@ -1,10 +1,24 @@
 import { routerRedux } from 'dva/router';
-import { stringify } from 'qs';
 import * as service from '@/services/login';
 import { setAuthority } from '@/utils/authority';
-import { getPageQuery } from '@/utils/utils';
 import { reloadAuthorized } from '@/utils/Authorized';
 import notificate from '@/utils/notification';
+
+function isLoginOrException(pathname) {
+  if (pathname === '/') return true;
+  if (pathname === '/user') return true;
+  if (pathname.startsWith('/user')) return true;
+  return false;
+}
+
+function getLastPage() {
+  const lastPage = window.localStorage.getItem('LAST_PAGE');
+  if (lastPage && !isLoginOrException(lastPage)) {
+    window.localStorage.removeItem('LAST_PAGE');
+    return lastPage;
+  }
+  return null;
+}
 
 export default {
   namespace: 'login',
@@ -24,21 +38,9 @@ export default {
           payload: { currentAuthority: 'admin' },
         });
         reloadAuthorized();
-        const urlParams = new URL(window.location.href);
-        const params = getPageQuery();
-        let { redirect } = params;
-        if (redirect) {
-          const redirectUrlParams = new URL(redirect);
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            redirect = null;
-          }
-        }
-        yield put(routerRedux.replace(redirect || '/'));
+
+        const lastPage = getLastPage();
+        yield put(routerRedux.replace(lastPage || '/'));
       }
 
       if (response.status === 404) notificate('warning', '警告', '用户名或密码出错！');
@@ -58,14 +60,7 @@ export default {
       reloadAuthorized();
       // redirect
       if (window.location.pathname !== '/user/login') {
-        yield put(
-          routerRedux.replace({
-            pathname: '/user/login',
-            search: stringify({
-              redirect: window.location.href,
-            }),
-          })
-        );
+        yield put(routerRedux.replace('/user/login'));
       }
     },
   },
@@ -78,6 +73,15 @@ export default {
         status: payload.status,
         type: payload.type,
       };
+    },
+  },
+
+  subscriptions: {
+    setup({ history }) {
+      history.listen(({ pathname }) => {
+        if (isLoginOrException(pathname)) return;
+        window.localStorage.setItem('LAST_PAGE', pathname);
+      });
     },
   },
 };
